dbe4df0c7893204c9277967f03fe43843d3e435f
ryananthonydiaz <email@ryananthonydiaz.com>
2019-09-18 09:52:37 -0700

    forkify

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+// export const key = '397a95d24ed464cf7d677c48f8a529e6';
+
+// export const key = '0d702b49f084fc72bf3b655dc8bfca93';
+
+// export const key = '9ea39bbe601768055f912a214149539e';
+
+export const key = 'a9ad762395b3f137a0c395d89fa4364f';
